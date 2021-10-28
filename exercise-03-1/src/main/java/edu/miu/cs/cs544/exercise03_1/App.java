e81469eb851d28e9830516366353f2ff192a85fe
Faisal Ahmed <faisalbegins@gmail.com>
2021-10-28 00:58:41 -0500

    refactored exercise03_1

--- exercise-03-1/src/main/java/edu/miu/cs/cs544/exercise03_1/App.java
+++ exercise-03-1/src/main/java/edu/miu/cs/cs544/exercise03_1/App.java
@@ -1,25 +1,12 @@
 package edu.miu.cs.cs544.exercise03_1;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-
 import java.util.List;
 
+import static edu.miu.cs.cs544.exercise03_1.HibernateUtils.withTx;
+
 public class App {
-
-	private static final SessionFactory sessionFactory;
-
-    static {
-		sessionFactory = HibernateUtils.getSessionFactory();
-	}
-
     public static void main(String[] args) {
-        Transaction tx = null;
-        try(Session session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
-
+        withTx(session -> {
             // crate car 1 and add owner
             Car chrysler = Car.of("Chrysler", "Pacifica2021", 58000);
             chrysler.setOwner(Owner.of("Faisal", "FairField"));
@@ -33,14 +20,7 @@ public class App {
             // get all the cars
             List<Car> cars = session.createQuery("from Car", Car.class).list();
             cars.forEach(System.out::println);
-
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) {
-                System.err.println("Rolling back: " + e.getMessage());
-                tx.rollback();
-            }
-        }
+        });
         System.exit(0);
     }
 }
